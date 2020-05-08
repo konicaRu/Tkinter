8728cbaf38acc05ec0fdfcfe1432e20d8b9e7e75
konica <prokol35@gmail.com>
2020-05-08 13:23:34 +0300

    08.05.20

--- Round_robin.py
+++ Round_robin.py
@@ -1,8 +1,10 @@
 from tkinter import *
+
 root = Tk()
 root.title('Round robin')  # надпись на верху
 root.geometry('500x300+300+200')  # ширина=500, высота=400, x=300, y=200 размер окна
 root.resizable(True, False)  # размер окна может быть изменён только по горизонтали
+
 
 class window_performer():
     def __init__(self, main):
@@ -16,34 +18,6 @@ class window_performer():
         self.listbox.grid(row=1, column=0)
 
 
-        main.bind('<Button-1>', self.click_on_citi)
-        """У функций-обработчиков, которые вызываются через bind(), а не через свойство command,
-должен быть обязательный параметр event, через который передается событие."""
-
-    def click_on_citi(self, event):
-        if self.state_varible.get() == 1:
-            cursor = list(self.listbox.curselection()) # Метод   curselection()   позволяет   получить   в   виде   кортежа   индексы   выбранных   элементов экземпляра Listbox.
-            for our_cities in cursor:
-                if self.list[our_cities] == "Москва":
-                    self.field_result['text'] = 'MCXLVII год'
-                if self.list[our_cities] == "Санкт-Петербург":
-                    self.field_result['text'] = 'MDCCIII год'
-                if self.list[our_cities] == "Саратов":
-                    self.field_result['text'] = 'MDXC год'
-                if self.list[our_cities] == "Омск":
-                    self.field_result['text'] = 'MDCCXVI год'
-        if self.state_varible.get() == 0:
-            cursor = list(self.listbox.curselection())
-            for our_cities in cursor:
-                if self.list[our_cities] == "Москва":
-                    self.field_result['text'] = '1147 год'
-                if self.list[our_cities] == "Санкт-Петербург":
-                    self.field_result['text'] = '1703 год'
-                if self.list[our_cities] == "Саратов":
-                    self.field_result['text'] = '1590 год'
-                if self.list[our_cities] == "Омск":
-                    self.field_result['text'] = '1716 год'
-
 class window_task():
     def __init__(self, main):
         self.listbox = Listbox(main, height=5, width=25, selectmode=EXTENDED)  # список с пуктами из листа list
@@ -55,25 +29,45 @@ class window_task():
         self.field_call.grid(row=0, column=1)
         self.listbox.grid(row=1, column=1)
 
+
 class window_work():
     def __init__(self, main):
         self.listbox = Listbox(main, height=5, width=25, selectmode=EXTENDED)  # список с пуктами из листа list
         self.list = ["Москва", "Санкт-Петербург", "Саратов", "Омск"]  # список пунктов в списке
         for i in self.list:
             self.listbox.insert(END, i)
-        self.field_call = Label(main, text='Список работ', width=18, font=10, justify=LEFT)
+        self.field_call = Label(main, text='Список работ', width=15, font=10, justify=LEFT)
 
         self.field_call.grid(row=0, column=2)
         self.listbox.grid(row=1, column=2)
 
 
+class New():
+    def __init__(self, main):
+        self.button_new = Button(main, text='New', width=16, font=10)
+        self.button_new.grid(row=3, column=0, sticky='s')
 
+
+class Setting():
+    def __init__(self, main):
+        self.button_set = Button(main, text='Setting', width=16, font=10, command=self.window_set)
+        self.button_set.grid(row=3, column=2)
+
+    def window_set(self):
+       self.window_open = Toplevel()
+       self.window_open.geometry('400x200')
+
+
+class Performer():
+    def __init__(self, name, arr_task):
+        self.name_performer = name
+        self.task_performer = arr_task #[]
 
 
 WindowPerformer = window_performer(root)
 WindowTask = window_task(root)
 WindowWork = window_work(root)
-
-
+ButtonNew = New(root)
+ButtonSetting = Setting(root)
 
 root.mainloop()
