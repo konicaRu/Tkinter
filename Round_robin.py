673e104d22d50f99dca0811618d8f586a108e794
konica <prokol35@gmail.com>
2020-05-08 16:25:03 +0300

    08.05.20

--- Round_robin.py
+++ Round_robin.py
@@ -48,20 +48,41 @@ class New():
         self.button_new.grid(row=3, column=0, sticky='s')
 
 
-class Setting():
+class Setting():# окно настройка
     def __init__(self, main):
         self.button_set = Button(main, text='Setting', width=16, font=10, command=self.window_set)
         self.button_set.grid(row=3, column=2)
 
-    def window_set(self):
-       self.window_open = Toplevel()
-       self.window_open.geometry('400x200')
+    def window_set(self):  # открываем окно с настройкаами
+        self.window_open = Toplevel()  # инициализируем новое окно
+        self.window_open.title('Setting')  # титул окна
+        self.window_open.geometry('600x200')  # размер окна
+        self.field_timer = Label(self.window_open, text='Время срабатывания таймера. сек ', borderwidth=3, width=30, font=10).grid(row=0, column=0)  # название поля ввода
+        self.entry_timer = Entry(self.window_open, width=8, font=15).grid(row=0, column=1)  # создаем окно ввода
+        self.field_number_perform_max = Label(self.window_open, text='Количество исполнителей. Максимальное:', borderwidth=3, width=35, font=10, justify='right').grid(row=1, column=0)  # название поля ввода
+        self.field_number_perform_max = Entry(self.window_open, width=8, font=15).grid(row=1, column=1)  # создаем окно ввода
+        self.field_number_perform_min = Label(self.window_open, text='Минимальное:', borderwidth=3, width=12, font=10, justify='right').grid(row=1, column=2)  # название поля ввода
+        self.field_number_perform_min = Entry(self.window_open, width=8, font=15).grid(row=1, column=3)  # создаем окно ввода
 
 
-class Performer():
-    def __init__(self, name, arr_task):
+        # self.button_ok = Button(self.window_open, text='OK', width=16, font=10).grid(row=1, column=0)  # создаем кнопку ОК
+        # self.button_cancel = Button(self.window_open, text='Cancel', width=16, font=10, command=self.close_win_setting).grid(row=2, column=0)  # создаем кнопку кенсел с командой закрытия окна
+
+    def close_win_setting(self):  # функция закрывающая окно
+        self.close_win_setting = self.window_open.destroy()  # команда закрывающая окно
+
+
+class Performer(): # класс исполнитель
+    def __init__(self, name, speed, arr_task):
         self.name_performer = name
-        self.task_performer = arr_task #[]
+        self.speed_performer = speed
+        self.task_performer = arr_task  # []
+
+
+class Task(): # класс задачи
+    def __init__(self, name, complex):
+        self.name_task = name
+        self.complexity_task = complex  # сложность задачи
 
 
 WindowPerformer = window_performer(root)
